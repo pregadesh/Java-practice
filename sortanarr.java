18cdf44b6d82c155180e6cbccd6e1ca65ba88abb
Pregadesh <144701889+pregadesh@users.noreply.github.com>
2025-09-18 05:41:44 +0530

    sortanarr.java
    Sorting an arr without build in sort()

---
+++ sortanarr.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class sortanarr{
+    public static void main(String[] args){
+        int[] arr = {10,9,8,7};
+        for (int i = 0; i<arr.length; i++){
+            for (int j = 0; j < arr.length-1-i; j++){
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+}
